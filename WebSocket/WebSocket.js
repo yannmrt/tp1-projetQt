caef97f242bb551c0b77b8ca38fb79b27a7eec09
Gregory-febvin <70692941+Gregory-febvin@users.noreply.github.com>
2021-09-23 09:18:40 +0200

    Envie de mourir

--- WebSocket/WebSocket.js
+++ WebSocket/WebSocket.js
@@ -12,32 +12,22 @@ socket.onerror = function (error) {
 
 
 socket.onopen = function (event) {
-
-    
     this.onmessage = function (msg) {
         this.send('MessageChat', msg);
     };
-
 };
-*/
+*/ 
 
-const express = require('express');
-const app = express();
-const http = require('http');
-const server = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server);
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+// Créer une connexion WebSocket
+const socket = new WebSocket('ws://192.168.65.230:178');
+
+// La connexion est ouverte
+socket.addEventListener('open', function (event) {
+ socket.send('Coucou le serveur !');
 });
 
-io.on('connection', (socket) => {
-  socket.on('MessageChat', (msg) => {
-    io.emit('MessageChat', msg);
-  });
-});
-
-server.listen(3000, () => {
-  console.log('Serveur en ligne');
+// Écouter les messages
+socket.addEventListener('message', function (event) {
+  console.log('Voici un message du serveur', event.data);
 });
