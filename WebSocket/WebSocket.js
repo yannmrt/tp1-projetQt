9a8491623486f49974ece84a07e36dbd9fdb63e2
Gregory-febvin <70692941+Gregory-febvin@users.noreply.github.com>
2021-09-23 12:01:15 +0200

    Add files via upload

--- WebSocket/WebSocket.js
+++ WebSocket/WebSocket.js
@@ -1,33 +1,15 @@
-/*
-var socket = null;
-try {
-    socket = new WebSocket("ws://192.168.65.230:178");
-} catch (exception) {
-    console.error(exception);
-}
-
-socket.onerror = function (error) {
-    console.error(error);
-};
-
-
-socket.onopen = function (event) {
-    this.onmessage = function (msg) {
-        this.send('MessageChat', msg);
-    };
-};
-*/ 
-
-
 // Créer une connexion WebSocket
-const socket = new WebSocket('ws://192.168.65.230:178');
+const socket = new WebSocket('ws://192.168.65.230:168');
 
+/*
 // La connexion est ouverte
 socket.addEventListener('open', function (event) {
- socket.send('Coucou le serveur !');
+ socket.send('truc');
 });
-
+*/
 // Écouter les messages
 socket.addEventListener('message', function (event) {
   console.log('Voici un message du serveur', event.data);
 });
+
+
