0d85bd721f0653bf6f4eefcd30053878e75ecd9d
Gregory-febvin <70692941+Gregory-febvin@users.noreply.github.com>
2021-09-26 16:38:13 +0200

    Ajout de commentaire

--- WebSocket/WebSocket.js
+++ WebSocket/WebSocket.js
@@ -6,16 +6,21 @@ const socket = new WebSocket('ws://192.168.65.230:168');
 socket.addEventListener('message', function (event) {
   console.log('Voici un message du serveur', event.data);
   
+  /*Si le server m'anonce que l'enregistrement est ok alors je vais afficher le formulaire de connexion*/
   if(event.data === "register.ok"){
     document.querySelector('.inscription').style.display='none';
     document.querySelector('.login').style.display='block';
-
+  }else{
+    alert("Imposible de vous inscrire");
   }
 
+  /*Si le server m'anonce que la connexion est ok alors je cache tout et j'affiche la boite de discusion*/
   if(event.data === "login.ok"){  
     document.querySelector('.inscription').style.display='none';
     document.querySelector('.login').style.display='none';
     document.querySelector(".MessageBox").style.display='flex';
+
+    /*Comme le login est correct, je vais attribuez la valeur de usernameConnexion pour la réutilisez*/
     UserCorrect = document.getElementById("usernameConnexion").value;
   }
 
@@ -24,5 +29,3 @@ socket.addEventListener('message', function (event) {
 
   
 });
-
-
