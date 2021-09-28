c18196d60280d8440de31853161fea4ebfb36d32
Yann martin <ymartin@la-providence.net>
2021-09-28 14:59:33 +0200

    compile tcpserver

--- TcpServer/ProjetQT/x64/Release/moc/moc_tcpserver.cpp
+++ TcpServer/ProjetQT/x64/Release/moc/moc_tcpserver.cpp
@@ -22,8 +22,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_TcpServer_t {
-    QByteArrayData data[6];
-    char stringdata0[83];
+    QByteArrayData data[5];
+    char stringdata0[72];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -36,13 +36,11 @@ QT_MOC_LITERAL(0, 0, 9), // "TcpServer"
 QT_MOC_LITERAL(1, 10, 21), // "onServerNewConnection"
 QT_MOC_LITERAL(2, 32, 0), // ""
 QT_MOC_LITERAL(3, 33, 20), // "onClientDisconnected"
-QT_MOC_LITERAL(4, 54, 17), // "onClientReadyRead"
-QT_MOC_LITERAL(5, 72, 10) // "sqlConnect"
+QT_MOC_LITERAL(4, 54, 17) // "onClientReadyRead"
 
     },
     "TcpServer\0onServerNewConnection\0\0"
-    "onClientDisconnected\0onClientReadyRead\0"
-    "sqlConnect"
+    "onClientDisconnected\0onClientReadyRead"
 };
 #undef QT_MOC_LITERAL
 
@@ -52,7 +50,7 @@ static const uint qt_meta_data_TcpServer[] = {
        8,       // revision
        0,       // classname
        0,    0, // classinfo
-       4,   14, // methods
+       3,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -60,13 +58,11 @@ static const uint qt_meta_data_TcpServer[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    0,   34,    2, 0x0a /* Public */,
-       3,    0,   35,    2, 0x0a /* Public */,
-       4,    0,   36,    2, 0x0a /* Public */,
-       5,    0,   37,    2, 0x0a /* Public */,
+       1,    0,   29,    2, 0x0a /* Public */,
+       3,    0,   30,    2, 0x0a /* Public */,
+       4,    0,   31,    2, 0x0a /* Public */,
 
  // slots: parameters
-    QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
@@ -83,7 +79,6 @@ void TcpServer::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, v
         case 0: _t->onServerNewConnection(); break;
         case 1: _t->onClientDisconnected(); break;
         case 2: _t->onClientReadyRead(); break;
-        case 3: _t->sqlConnect(); break;
         default: ;
         }
     }
@@ -119,13 +114,13 @@ int TcpServer::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 4)
+        if (_id < 3)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 4;
+        _id -= 3;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 4)
+        if (_id < 3)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 4;
+        _id -= 3;
     }
     return _id;
 }
