3ba88c610bf26ad15f99ba91ddfa644688a89e4a
Yann martin <ymartin@la-providence.net>
2021-09-28 14:58:57 +0200

    Compile webserver

--- WebServer/WebServer/x64/Release/moc/moc_WebServer.cpp
+++ WebServer/WebServer/x64/Release/moc/moc_WebServer.cpp
@@ -21,33 +21,39 @@
 QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
-struct qt_meta_stringdata_WebSocket_t {
-    QByteArrayData data[3];
-    char stringdata0[33];
+struct qt_meta_stringdata_WebServer_t {
+    QByteArrayData data[7];
+    char stringdata0[92];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
-    qptrdiff(offsetof(qt_meta_stringdata_WebSocket_t, stringdata0) + ofs \
+    qptrdiff(offsetof(qt_meta_stringdata_WebServer_t, stringdata0) + ofs \
         - idx * sizeof(QByteArrayData)) \
     )
-static const qt_meta_stringdata_WebSocket_t qt_meta_stringdata_WebSocket = {
+static const qt_meta_stringdata_WebServer_t qt_meta_stringdata_WebServer = {
     {
-QT_MOC_LITERAL(0, 0, 9), // "WebSocket"
-QT_MOC_LITERAL(1, 10, 21), // "onServerNewConnection"
-QT_MOC_LITERAL(2, 32, 0) // ""
+QT_MOC_LITERAL(0, 0, 9), // "WebServer"
+QT_MOC_LITERAL(1, 10, 13), // "newConnection"
+QT_MOC_LITERAL(2, 24, 0), // ""
+QT_MOC_LITERAL(3, 25, 18), // "processTextMessage"
+QT_MOC_LITERAL(4, 44, 7), // "message"
+QT_MOC_LITERAL(5, 52, 18), // "socketDisconnected"
+QT_MOC_LITERAL(6, 71, 20) // "processBinaryMessage"
 
     },
-    "WebSocket\0onServerNewConnection\0"
+    "WebServer\0newConnection\0\0processTextMessage\0"
+    "message\0socketDisconnected\0"
+    "processBinaryMessage"
 };
 #undef QT_MOC_LITERAL
 
-static const uint qt_meta_data_WebSocket[] = {
+static const uint qt_meta_data_WebServer[] = {
 
  // content:
        8,       // revision
        0,       // classname
        0,    0, // classinfo
-       1,   14, // methods
+       4,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -55,63 +61,71 @@ static const uint qt_meta_data_WebSocket[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    0,   19,    2, 0x0a /* Public */,
+       1,    0,   34,    2, 0x0a /* Public */,
+       3,    1,   35,    2, 0x0a /* Public */,
+       5,    0,   38,    2, 0x0a /* Public */,
+       6,    0,   39,    2, 0x0a /* Public */,
 
  // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::QString,    4,
+    QMetaType::Void,
     QMetaType::Void,
 
        0        // eod
 };
 
-void WebSocket::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+void WebServer::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
 {
     if (_c == QMetaObject::InvokeMetaMethod) {
-        auto *_t = static_cast<WebSocket *>(_o);
+        auto *_t = static_cast<WebServer *>(_o);
         Q_UNUSED(_t)
         switch (_id) {
-        case 0: _t->onServerNewConnection(); break;
+        case 0: _t->newConnection(); break;
+        case 1: _t->processTextMessage((*reinterpret_cast< QString(*)>(_a[1]))); break;
+        case 2: _t->socketDisconnected(); break;
+        case 3: _t->processBinaryMessage(); break;
         default: ;
         }
     }
-    Q_UNUSED(_a);
 }
 
-QT_INIT_METAOBJECT const QMetaObject WebSocket::staticMetaObject = { {
+QT_INIT_METAOBJECT const QMetaObject WebServer::staticMetaObject = { {
     QMetaObject::SuperData::link<QMainWindow::staticMetaObject>(),
-    qt_meta_stringdata_WebSocket.data,
-    qt_meta_data_WebSocket,
+    qt_meta_stringdata_WebServer.data,
+    qt_meta_data_WebServer,
     qt_static_metacall,
     nullptr,
     nullptr
 } };
 
 
-const QMetaObject *WebSocket::metaObject() const
+const QMetaObject *WebServer::metaObject() const
 {
     return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
 }
 
-void *WebSocket::qt_metacast(const char *_clname)
+void *WebServer::qt_metacast(const char *_clname)
 {
     if (!_clname) return nullptr;
-    if (!strcmp(_clname, qt_meta_stringdata_WebSocket.stringdata0))
+    if (!strcmp(_clname, qt_meta_stringdata_WebServer.stringdata0))
         return static_cast<void*>(this);
     return QMainWindow::qt_metacast(_clname);
 }
 
-int WebSocket::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+int WebServer::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
 {
     _id = QMainWindow::qt_metacall(_c, _id, _a);
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 1)
+        if (_id < 4)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 1;
+        _id -= 4;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 1)
+        if (_id < 4)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 1;
+        _id -= 4;
     }
     return _id;
 }
