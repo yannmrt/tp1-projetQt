023f49b34b1ee524d23b5247ba8a3bacf39c1464
yannmrt <70692715+yannmrt@users.noreply.github.com>
2021-09-30 09:05:37 +0200

    Add files via upload

---
+++ WebServer/WebServer/WebServer.h
@@ -0,0 +1,40 @@
+#include <QtWidgets/QMainWindow>
+#include "ui_WebServer.h"
+#include "database.h"
+
+#include <QtWebSockets/QtWebSockets>
+
+#include <QtCore/QObject>
+#include <QtCore/QList>
+#include <QtCore/QByteArray>
+
+#include <QtSql/QtSql>
+#include <QApplication>
+#include <QDateTime>
+
+QT_FORWARD_DECLARE_CLASS(QWebSocketServer)
+QT_FORWARD_DECLARE_CLASS(QWebSocket)
+
+class WebServer : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    WebServer(QWidget *parent = Q_NULLPTR);
+	char requete;
+	char retour;
+	char donnees;
+
+private:
+    Ui::WebServerClass ui;
+	QWebSocketServer *m_pWebSocketServer;
+	QList<QWebSocket *> m_clients;
+	BaseDeDonnees *bddMySQL;
+	bool m_debug;
+
+public slots:
+	void newConnection();
+	void processTextMessage(QString message);
+	void socketDisconnected();
+	void processBinaryMessage();
+};
